0aeb50728ace2a9bdc2b1d4f4b3999322a68f9d4
=sarad khadayat <=saradkhadayat1807@gmail.com>
2023-09-13 07:57:59 +0545

    jquery_Practise

--- jquery/jquery_practise.js
+++ jquery/jquery_practise.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
     $("button").click(function(){
       $(".paraClass").hide();
+      $.get("https://reqres.in/api/users?page=1",function(response, status){
+        console.log(status);
+        if(status =="success"){
+          console.log(response['page']); 
+        }else{
+          $("#apifailure").text('error loading');
+        }
+      });
     });
   });
